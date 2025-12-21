277624c6334aed26c0860176f4e5b693e6649b07
Soona <code@chainsona.dev>
2025-12-22 00:14:33 +0100

    style: add top padding to cluster status content to prevent clipping of first item

--- src/components/cluster-status.tsx
+++ src/components/cluster-status.tsx
@@ -135,7 +135,7 @@ export function ClusterStatus({ nodes, className }: ClusterStatusProps) {
           </div>
         </CardTitle>
       </CardHeader>
-      <CardContent className="px-5 pt-0 pb-3 flex-1 flex flex-col min-h-0 justify-between overflow-hidden">
+      <CardContent className="px-5 pt-2 pb-3 flex-1 flex flex-col min-h-0 justify-between overflow-hidden">
         <div className="space-y-1.5 flex-1 flex flex-col justify-center overflow-y-auto min-h-0 py-1">
           {layers.map((layer) => {
             const Icon = layer.icon;
